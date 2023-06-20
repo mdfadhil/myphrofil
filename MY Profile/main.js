4282777a5cdd19ec738329d3054e6fb63d442137
mdfadhil <135823165+mdfadhil@users.noreply.github.com>
2023-06-20 16:02:57 +0530

    Add files via upload

---
+++ MY Profile/main.js
@@ -0,0 +1,10 @@
+// var typed new typed('.text',{
+//     String:["Frontend developer","Youtuber","web developer"]
+//     typeSpeed:100,
+//     backSpeed:100,
+//     backDelay:1000,
+//     loop:true;
+// })
+
+
+
